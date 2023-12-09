a0767d22a9b1b4ffcc308b2b0dc0ddc9cce91161
Jawid Fakhari <jawid_fakhari@live.it>
2023-12-09 16:59:27 +0100

    localSotrage-added

--- script.js
+++ script.js
@@ -8,17 +8,27 @@ button.addEventListener("click", () => {
   } else {
     let newTask = `<li>${inputBox.value}<span>🗑️</span></li>`;
     listContainer.insertAdjacentHTML("afterbegin", newTask);
-    inputBox.value = "";
   }
+  inputBox.value = "";
+  storeData();
 });
 
 listContainer.addEventListener("click", (e) => {
   if (e.target.tagName === "LI") {
     e.target.classList.toggle("checked");
+    storeData();
   } else if (e.target.tagName === "SPAN") {
     e.target.parentElement.remove();
+    storeData();
   }
 });
 
-//salvare i dati nel local storage
+//save in local storage then call it
+function storeData() {
+  localStorage.setItem("data", listContainer.innerHTML);
+}
+function showData() {
+  listContainer.innerHTML = localStorage.getItem("data");
+}
+showData();
 //creare un estensione per google
